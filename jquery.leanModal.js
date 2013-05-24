bd52068f8583129d5d0db72cad0af9a07af3ca31
aufula <aufula@hotmail.com>
2013-05-24 16:24:57 +0800

    bugfix: wrong position

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -16,71 +16,77 @@
  * usage
  * ===================
  * $(".trigger_selector").leanModal();
- * 
+ *
  */
 
-(function($){
+(function($) {
 
-    $.fn.extend({
+	$.fn.extend({
 
-        leanModal: function(options) {
-            var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null,
-                posType: 'fixed'
-            }
+		leanModal: function(options) {
+			var defaults = {
+				top: 100,
+				overlay: 0.5,
+				closeButton: null,
+				posType: 'fixed'
+			}
 
-            $("body").append("<div id='lean_overlay'></div>");
-            var options =  $.extend(defaults, options);
+			$("body").append("<div id='lean_overlay'></div>");
+			var options = $.extend(defaults, options);
 
-            return this.each(function() {
-                var o = options;
-                var posTop = o.top;
+			return this.each(function() {
+				var o = options;
+				var posTop = o.top;
 
-                $(this).click(function(e) {
-                    var layer =  $("#lean_overlay");
+				$(this).click(function(e) {
+					var layer = $("#lean_overlay");
 
-                    if( jQuery.isFunction(posTop) && posTop != 'fixed' ){
-                        posTop = o.top();
-                    };
+					if (jQuery.isFunction(o.top) && posTop != 'fixed') {
+						posTop = o.top();
+					};
 
-                    var modal_id = $(this).attr("href");
+					var modal_id = $(this).attr("href");
 
-                    layer.click(function() {
-                        close_modal(modal_id);
-                    });
+					layer.click(function() {
+						close_modal(modal_id);
+					});
 
-                    $(o.closeButton).click(function() {
-                        close_modal(modal_id);
-                    });
+					$(o.closeButton).click(function() {
+						close_modal(modal_id);
+					});
 
-                    var modal_height = $(modal_id).outerHeight();
-                    var modal_width = $(modal_id).outerWidth();
+					var modal_height = $(modal_id).outerHeight();
+					var modal_width = $(modal_id).outerWidth();
 
-                    layer.css({ 'display' : 'block', opacity : 0 }).fadeTo(200,o.overlay);
+					layer.css({
+						'display': 'block',
+						opacity: 0
+					}).fadeTo(200, o.overlay);
 
-                    $(modal_id).css({
-                        'display' : 'block',
-                        'position' : o.posType,
-                        'opacity' : 0,
-                        'z-index': 11000,
-                        'left' : 50 + '%',
-                        'margin-left' : -(modal_width/2) + "px",
-                        'top' : posTop + "px"
-                    }).fadeTo(200,1);
+					$(modal_id).css({
+						'display': 'block',
+						'position': o.posType,
+						'opacity': 0,
+						'z-index': 11000,
+						'left': 50 + '%',
+						'margin-left': - (modal_width / 2) + "px",
+						'top': posTop + "px"
+					}).fadeTo(200, 1);
 
-                    e.preventDefault();
-                });
+					e.preventDefault();
+				});
 
-            });
+			});
 
-            function close_modal(modal_id){
-                $("#lean_overlay").fadeOut(200);
-                $(modal_id).css({ 'display' : 'none' });
-            }
+			function close_modal(modal_id) {
+				$("#lean_overlay").fadeOut(200);
+				$(modal_id).css({
+					'display': 'none'
+				});
+			}
 
-        }
-    });
+		}
+	});
 
 })(jQuery);
+
